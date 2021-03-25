3e910cc48dbba3fe917e05c65d528c3e84919574
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-03-25 17:30:29 -0300

    using boolean in ts

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else return n1 + n2;
 }
 
 // here assigning types make that our code do not compile so that the mistake of adding a string and a number don't return a concatenation
 
 const number1 = 5; // to fix this we simply pass the right type
 const number2 = 2.8;
+const printResult = true;
 
 //or convert to a number
-const result = add(Number(number1), +number2); // Argument of type 'string' is not assignable to parameter of type 'number'.
-console.log(result);
+const result = add(Number(number1), +number2, printResult); // Argument of type 'string' is not assignable to parameter of type 'number'.
