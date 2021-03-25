15776b31ad21d0490eb0725471fdd21694c38e96
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-03-25 17:20:45 -0300

    using right type or converting

---
+++ app.js
@@ -0,0 +1,9 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+// here assigning types make that our code do not compile so that the mistake of adding a string and a number don't return a concatenation
+var number1 = 5; // to fix this we simply pass the right type
+var number2 = 2.8;
+//or convert to a number
+var result = add(Number(number1), +number2); // Argument of type 'string' is not assignable to parameter of type 'number'.
+console.log(result);
--- app.ts
+++ app.ts
@@ -3,8 +3,10 @@ function add(n1: number, n2: number) {
 }
 
 // here assigning types make that our code do not compile so that the mistake of adding a string and a number don't return a concatenation
-const number1 = '5';
+
+const number1 = 5; // to fix this we simply pass the right type
 const number2 = 2.8;
 
-const result = add(number1, number2); // Argument of type 'string' is not assignable to parameter of type 'number'.
+//or convert to a number
+const result = add(Number(number1), +number2); // Argument of type 'string' is not assignable to parameter of type 'number'.
 console.log(result);
