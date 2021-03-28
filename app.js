5e979d40d81053950314212110c995de4c6dc5dd
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-03-28 17:03:16 -0300

    using enums

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+var RoleEnum;
+(function (RoleEnum) {
+    RoleEnum[RoleEnum["ADMIN"] = 0] = "ADMIN";
+    RoleEnum[RoleEnum["READ_ONLY"] = 1] = "READ_ONLY";
+    RoleEnum[RoleEnum["AUTHOR"] = 2] = "AUTHOR";
+})(RoleEnum || (RoleEnum = {}));
 var person = {
-  name: 'Lucas',
-  age: 30,
-  hobbies: ['Sports', 'Music', 'Cooking'],
+    name: 'Lucas',
+    age: 30,
+    hobbies: ['Sports', 'Music', 'Cooking'],
+    role: RoleEnum.ADMIN
 };
 console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-  var hobby = _a[_i];
-  console.log(hobby.toUpperCase());
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+if (person.role === RoleEnum.ADMIN) {
+    console.log('is an admin');
 }
--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
+enum RoleEnum {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: 'Lucas',
   age: 30,
-  hobbies: ['Sports', 'Music', 'Cooking'], // in ts an array have a type in this case string[] meaning that is an array of strings
-  // is best practice to not mix types in one array but we can do it with any[]
+  hobbies: ['Sports', 'Music', 'Cooking'],
+  role: RoleEnum.ADMIN,
 };
 
 console.log(person);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase()); // here we have a great auto completion because the ts compiler knows that every hobby is a string.
+  console.log(hobby.toUpperCase());
+}
 
-  // console.log(hobby.map()); // Property 'map' does not exist on type 'string'.
+if (person.role === RoleEnum.ADMIN) {
+  console.log('is an admin');
 }
