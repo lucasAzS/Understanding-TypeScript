cf7dcc357b5b25522487d8e69ed7882d89184354
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-03-28 20:10:03 -0300

    Functions as types

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ function printResult(num) {
     console.log('Result: ' + num);
 }
 printResult(add(1, 2));
+var combineValues; // here we say exactly what type of function can be assigned here
+combineValues = add;
+// combineValues = printResult; // Type '(num: number) => void' is not assignable to type '(a: number, b: number) => number'.
+// Type 'void' is not assignable to type 'number'.
+// combineValues = 5; // Type 'number' is not assignable to type 'Function'.
+console.log(combineValues(3, 2));
--- app.ts
+++ app.ts
@@ -7,3 +7,11 @@ function printResult(num: number) {
 }
 
 printResult(add(1, 2));
+
+let combineValues: (a: number, b: number) => number; // here we say exactly what type of function can be assigned here
+combineValues = add;
+// combineValues = printResult; // Type '(num: number) => void' is not assignable to type '(a: number, b: number) => number'.
+// Type 'void' is not assignable to type 'number'.
+
+// combineValues = 5; // Type 'number' is not assignable to type 'Function'.
+console.log(combineValues(3, 2));
