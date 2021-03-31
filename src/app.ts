e40f69973b54fb90431e0c0ac53c223bc21542aa
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-03-30 21:12:43 -0300

    first class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('hi');
+class Departament {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accounting = new Departament('Accounting');
+console.log(accounting);
