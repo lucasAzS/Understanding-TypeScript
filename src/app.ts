278fb1f2591df83ef49d0215d6d687a180c1d15a
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-04-11 05:41:44 -0300

    Inheritance

--- src/app.ts
+++ src/app.ts
@@ -23,14 +23,43 @@ class Departament {
   }
 }
 
-const accounting = new Departament('d1', 'Accounting');
+class ITDepartament extends Departament {
+  admins: string[]; /* Just to make clear that when using the constructor or use this. in 'child' class we need to use super() */
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee('Lucas');
-accounting.addEmployee('Lulu');
+class AccountingDepartament extends Departament {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
 
-accounting.decribe();
-// accounting.name = 'other name' // Cannot assign to 'name' because it is a read-only property.
-accounting.printEmployInfo();
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const t1 = new ITDepartament('t1', ['lucas', 'lulu']);
+t1.addEmployee('Lucas');
+t1.addEmployee('Lulu');
+
+t1.decribe();
+// t1.name = 'other name' // Cannot assign to 'name' because it is a read-only property.
+t1.printEmployInfo();
 // const accountingCopy = { name: 'wrong name :)', describe: accounting.decribe };
 
 // accountingCopy.describe(); // error
+
+console.log(t1);
+
+const accounting = new AccountingDepartament('d2', []);
+
+accounting.addReport('Something happened...');
+accounting.printReports();
+
+console.log('The Accounting dep: ', accounting);
