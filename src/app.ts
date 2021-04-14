f14202dd723b5abdae497e20b45f636727308a26
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-04-13 22:29:17 -0300

    readonly interfaces props

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 interface Greetable {
-  name: string;
+  readonly name: string;
 
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  constructor(public name: string, public age: number) {}
+  name: string;
+  constructor(n: string, public age: number) {
+    this.name = n;
+  }
 
   greet(phrase: string) {
     console.log(phrase + '' + this.name);
   }
 }
 
-let user1 = new Person('Lucas', 26);
+let user1 = new Person('Lulucas', 26);
 
 user1.greet('Hi my name is ');
+user1.name = 'roberto'; // the name is not changed bcs is read-only
